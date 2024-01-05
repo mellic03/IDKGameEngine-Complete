af3c803bbe8691b3247cfacf8997594588d062dc
mellic03 <mellic03@gmail.com>
2024-01-05 15:01:14 +1000

    CMake changes

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-mkdir -p build/{CMake,output}
+mkdir -p build/CMake
+mkdir -p build/CMake/{debug,release}
 
-cd build/CMake
-cmake -G Ninja ../../
-ninja -j 8
+cd build/CMake/debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_IDK_DEBUG=ON -G Ninja ../../../
+ninja -j 12
 
 
-cd ../
-cp -R ../IDKGameEngine/IDKGE ./output/.
-cp -R ../IDKGame/assets ./output/.
+cd ../release
+# cmake -DCMAKE_BUILD_TYPE=Release -DUSE_IDK_DEBUG=OFF -G Ninja ../../../
+# ninja -j 12
+
+
+
+cd ../../
+cp -R ../IDKRenderEngine/IDKGE ./debug/.
+cp -R ../IDKGame/assets        ./debug/.
+cp -R ../IDKEditorUI/assets    ./debug/.
+
+
+# cp -R ../IDKRenderEngine/IDKGE ./release/.
+# cp -R ../IDKGame/assets        ./release/.
+# cp -R ../IDKEditorUI/assets    ./release/.
