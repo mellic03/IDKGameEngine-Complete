4732998b4763fc4c570a293c8caf41ce3178ea0c
mellic03 <mellic03@gmail.com>
2024-01-14 14:48:04 +1000

    Directory restructure

--- build.sh
+++ build.sh
@@ -6,20 +6,27 @@ mkdir -p build/CMake/{debug,release}
 cd build/CMake/debug
 cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_IDK_DEBUG=ON -G Ninja ../../../
 ninja -j 12
+cmake --install .
 
 
 cd ../release
 # cmake -DCMAKE_BUILD_TYPE=Release -DUSE_IDK_DEBUG=OFF -G Ninja ../../../
 # ninja -j 12
+# cmake --install .
 
 
 
 cd ../../
-cp -R ../IDKGraphics/IDKGE ./debug/.
-cp -R ../IDKGame/assets        ./debug/.
-cp -R ../IDKBuiltinUI/assets   ./debug/.
+cp -R ../IDKGraphics/IDKGE  ./debug/.
+cp -R ../IDKGame/assets     ./debug/.
+# cp -R ../IDKBuiltin/assets  ./debug/.
 
 
-# cp -R ../IDKGraphics/IDKGE ./release/.
-# cp -R ../IDKGame/assets        ./release/.
-# cp -R ../IDKEditorUI/assets    ./release/.
+# cp -R ../IDKGraphics/IDKGE  ./release/.
+# cp -R ../IDKGame/assets     ./release/.
+# cp -R ../IDKBuiltin/assets  ./release/.
+
+
+
+cp ./lib/* ./debug/IDKGE/runtime/.
+# cp ./lib/* ./release/IDKGE/runtime/.
