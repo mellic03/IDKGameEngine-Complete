c4acb5d6f22c61d9a7a1330d3f972a3b32ef2915
mellic03 <mellic03@gmail.com>
2024-09-12 16:08:06 +1000

    submodule updates

--- build.sh
+++ build.sh
@@ -8,15 +8,17 @@ mkdir -p build/{debug,release}
 cd build/CMake/debug
 cmake -D CMAKE_BUILD_TYPE=DEBUG -D IDK_DEBUG=ON -G Ninja ../../../
 ninja
-# cmake --install .
 
 cd ../release
 # cmake -D CMAKE_BUILD_TYPE=RELEASE -G Ninja ../../../
 # ninja
-# cmake --install .
 
 
-cd ../../
+cd ../../../
+rm -R ./MEATWORLD/meatworldgame/src/shaders/include
+cp -R ./IDKGraphics/IDKGE/shaders/include ./MEATWORLD/meatworldgame/src/shaders/include
+
+cd ./build
 mkdir -p ./debug/IDKGE/runtime/modules
 cp ./lib/debug/*.so ./debug/IDKGE/runtime/.
 mv ./debug/IDKGE/runtime/libIDKBuiltin* ./debug/IDKGE/runtime/modules/.
